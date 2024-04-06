fc5e0c220dd1eeac20a945ae3ea9fdb8a4da035c
Nardos Tsegaye <nardossts@gmail.com>
2024-04-06 05:15:51 +0300

    setting client project and add typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const App = () => {
+  return <div>Hello, world! yuck</div>;
+};
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    hot: true,
+    open: true,
+  },
+};
